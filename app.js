5640a7121f8f1b6fc2a420c7fe8816ec9b10c994
olga <oluciams84@gmail.com>
2021-06-27 13:11:51 -0500

    Buscando en el encabezado el tipo de navegador que utiliza el cliente cuando se hace una peticion (headers user-agent)

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+
+
+app.get('/header', (req, res) => {
+    let header = req.headers['user-agent']
+    res.send(`<p>El navegador es: ${header}`);
+});
+
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
